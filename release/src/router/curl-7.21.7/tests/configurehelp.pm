8f4f55d38fe497376bdf6d428194ff3e47c528bd
zhoutao0712 <846207054@qq.com>
2018-05-29 11:49:51 +0800

    support libcurl

---
+++ release/src/router/curl-7.21.7/tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = 'mipsel-uclibc-gcc -E -I/root/Desktop/yhwrt/release/src-ra-4300/router/openssl/include -I/root/Desktop/yhwrt/release/src-ra-4300/router/openssl/include/openssl';
+
+1;
